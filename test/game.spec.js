eb2025f1a99ecaa87c450b8a39b3d75ff6fda4b3
Marco Casula <mc@clickbit.net>
2016-10-19 15:53:07 -0500

    introduce "presets.js"; add more tests
    Tests can now run using a specific set of rules.
    You can easily decide which combination of cards and actions
    to run against a specific set of rules.
    
    example:
    
    cards: '♥9 ♦K ♥3 ♣3 ♠1 ♦1',
    actions: ['restore', 'deal', 'split'],
    using showdownAfterAceSplit `true` or `false`
    
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- test/game.spec.js
+++ test/game.spec.js
@@ -18,12 +18,13 @@
 
 const assert = require('assert')
 const Game = require('../src/game')
+const presets = require('../src/presets')
 const actions = require('../src/actions')
 const engine = require('../src/engine')
-const util = require('util')
 
-const executeFlow = (cards, activity) => {
-  const game = new Game()
+
+const executeFlow = (rules = {} , cards, activity) => {
+  const game = new Game(null, presets.getRules(rules))
   let status = game.getState()
   status.deck = status.deck.concat(engine.serializeCards(cards))
   activity.forEach(fn => {
@@ -40,7 +41,7 @@ const functions = {
   'split': () => actions.split(),
   'hitR': () => actions.hit({position: 'right'}),
   'standR': () => actions.stand({position: 'right'}),
-  'standL': () => actions.stand({position: 'left'}),
+  'standL': () => actions.stand({position: 'left'})
 }
 
 describe('Game flow', function () {
@@ -63,14 +64,36 @@ describe('Game flow', function () {
         actions: ['restore', 'deal', 'split', 'standR'],
         stage: 'player-turn-left',
         finalWin: 0
+      },
+      {
+        // force showdown after splitting ace rule ON.
+        // in this case we want to terminate the game even if right side has 20
+        cards: '♥9 ♦K ♥3 ♣3 ♠1 ♦1',
+        actions: ['restore', 'deal', 'split'],
+        stage: 'done',
+        finalWin: 0,
+        rules: {
+          showdownAfterAceSplit: true
+        }
+      },
+      {
+        // force showdown after splitting ace rule OFF.
+        // in this case the game is not closed on left
+        cards: '♥9 ♦K ♥3 ♣3 ♠1 ♦1',
+        actions: ['restore', 'deal', 'split'],
+        stage: 'player-turn-right',
+        finalWin: 0,
+        rules: {
+          showdownAfterAceSplit: false
+        }
       }
     ].forEach(test => {
       it(`should deal ${test.cards} execute ${test.actions.join('-')} and finish`, function () {
-        const state = executeFlow(test.cards, test.actions.map(x => functions[x]))
-        if (test.stage){
+        const state = executeFlow(test.rules, test.cards, test.actions.map(x => functions[x]))
+        if (test.stage) {
           assert.equal(state.stage, test.stage, test.cards)
         }
-        if (test.finalWin){
+        if (test.finalWin) {
           assert.equal(state.finalWin, test.finalWin)
         }
       })
