9d440353611f2cdf10ee5e1169e397a7b779c1bc
Marco Casula <mc@clickbit.net>
2017-03-03 11:30:05 -0500

    add neutrino to impl build process and babel
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node examples/basicUI/miniserver.js
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*!
+ * engine-blackjack
+ * Copyright(c) 2016 Marco Casula
+ * GPL 2.0 Licensed
+ */
+
+'use strict'
+
+module.exports = {
+  constants: require('./constants'),
+  engine: require('./engine'),
+  presets: require('./presets'),
+  actions: require('./actions'),
+  Game: require('./game')
+}
