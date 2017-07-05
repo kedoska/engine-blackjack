da76c7ec0e400b70e2bbb4010736a338361ed851
Marco Casula <mc@clickbit.net>
2017-07-05 13:14:17 -0500

    impl stats for Dealer bust after deal and player stand
    in ref #55
    
    Following the information here
    http://www.blackjackage.com/bust-out-rate.php
    
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- test/stress.spec.js
+++ test/stress.spec.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-import { calculate, isBlackjack } from '../src/engine'
+import { calculate, isBlackjack, getHigherValidValue } from '../src/engine'
 const deck = require('52-deck')
 deck.getRandom = (min, max) => {
   let number
@@ -14,39 +14,75 @@ deck.getRandom = (min, max) => {
   return number + min
 }
 
-const simulate = (decks = 1) => {
-  const cards = deck.shuffle(deck.newDecks(decks))
-  const playerCards = cards.splice(cards.length - 2, 2)
-  const dealerFirstCards = cards.splice(cards.length - 1, 1)
-  const dealerHoleCard = cards.splice(cards.length - 1, 1)[ 0 ]
-  const dealerCards = dealerFirstCards.concat([dealerHoleCard])
-  const playerHasBlackjack = isBlackjack(playerCards)
-  const dealerHasBlackjack = isBlackjack(dealerCards)
-  return {
-    playerHasBlackjack,
-    dealerHasBlackjack
-  }
-}
-
-describe.only('Probabilities', () => {
+describe('Probabilities', () => {
   describe('Served blackjack', () => {
-    [1,2,4,6,8].forEach(deck => {
-      it(`should be around 4% with ${deck} deck/s`, () => {
+    [1,2,4,6,8].forEach(numberOfDecks => {
+      it(`should be around 4% with ${numberOfDecks} deck/s`, () => {
         const MAX = 10000
         let p = 0
         let d = 0
         for(let i = 0; i < MAX; i ++) {
-          const { playerHasBlackjack, dealerHasBlackjack } = simulate(deck)
+          const cards = deck.shuffle(deck.newDecks(numberOfDecks))
+          const playerCards = cards.splice(cards.length - 2, 2)
+          const dealerFirstCards = cards.splice(cards.length - 1, 1)
+          const dealerHoleCard = cards.splice(cards.length - 1, 1)[ 0 ]
+          const dealerCards = dealerFirstCards.concat([dealerHoleCard])
+          const playerHasBlackjack = isBlackjack(playerCards)
+          const dealerHasBlackjack = isBlackjack(dealerCards)
           p = playerHasBlackjack ? p + 1 : p
           d = dealerHasBlackjack ? d + 1 : d
         }
         p = (p / MAX) * 100
-        d = (d/ MAX) * 100
+        d = (d / MAX) * 100
         expect(p).toBeGreaterThanOrEqual(4)
         expect(p).toBeLessThanOrEqual(6)
 
         expect(d).toBeGreaterThanOrEqual(4)
         expect(d).toBeLessThanOrEqual(6)
+      })
+    })
+  })
+  describe('Dealer bust after deal and player stand', () => {
+    [1,2,4,6,8].forEach(numberOfDecks => {
+      it(`should be between 22 and 32% with ${numberOfDecks} deck/s`, () => {
+        const MAX = 1000
+        let numberOfBustedGames = 0
+        let numberOfDealerWinningGames = 0
+        let numberOfPlayerWinningGames = 0
+        let numberOfPushGames = 0
+        for (let i = 0; i < MAX; i++) {
+          const cards = deck.shuffle(deck.newDecks(numberOfDecks))
+          const playerCards = cards.splice(cards.length - 2, 2)
+          const dealerFirstCards = cards.splice(cards.length - 1, 1)
+          const dealerHoleCard = cards.splice(cards.length - 1, 1)[0]
+          let dealerCards = dealerFirstCards.concat([dealerHoleCard])
+          const pV = getHigherValidValue(calculate(playerCards))
+          let dV = getHigherValidValue(calculate(dealerCards))
+          while (dV < 17){
+            const nextCard = cards.splice(cards.length - 1, 1)[0]
+            dealerCards = dealerCards.concat([nextCard])
+            dV = getHigherValidValue(calculate(dealerCards))
+          }
+          let dealerHasBusted = dV > 21
+
+          if (dealerHasBusted) {
+            numberOfBustedGames++
+          }
+          if (!dealerHasBusted && dV > pV) {
+            numberOfDealerWinningGames++
+          }
+          if (pV > dV || dealerHasBusted) {
+            numberOfPlayerWinningGames++
+          }
+          if (!dealerHasBusted && dV === pV) {
+            numberOfPushGames++
+          }
+        }
+
+        numberOfBustedGames = (numberOfBustedGames / MAX) * 100
+
+        expect(numberOfBustedGames).toBeGreaterThanOrEqual(22)
+        expect(numberOfBustedGames).toBeLessThanOrEqual(32)
       })
     })
   })
